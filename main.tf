f29b172ae7298050cb97719112f74e557f63461a
Oni <oniabifo@gmail.com>
2024-08-15 10:55:46 -0400

    Azure file share

--- main.tf
+++ main.tf
@@ -3,21 +3,28 @@ resource "azurerm_resource_group" "rg" {
   location = "centralus"
 }
 
-resource "azurerm_storage_account" "sa" {
+resource "azurerm_storage_account" "aspire" {
   name                     = "aspirestorage"  # Ensure the name is globally unique
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  large_file_share_enabled = true
 }
 
-resource "azurerm_storage_container" "container" {
-  name                  = "content-container"
-  storage_account_name  = azurerm_storage_account.sa.name
-  container_access_type = "private"
-}
+resource "azurerm_storage_share" "example" {
+  name                 = "aspire_share"
+  storage_account_name = azurerm_storage_account.aspire.name
+  quota                = 5000
 
+  acl {
+    id = "MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI"
 
-output "blob_storage_url" {
-  value = "https://${azurerm_storage_account.sa.name}.blob.core.windows.net/${azurerm_storage_container.container.name}/"
+    access_policy {
+      permissions = "rwdl"
+      start       = "2019-07-02T09:38:21.0000000Z"
+      expiry      = "2030-07-02T10:38:21.0000000Z"
+    }
+  }
+  enabled_protocol = "NFS"
 }
